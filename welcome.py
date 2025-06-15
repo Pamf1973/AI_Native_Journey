03ef3991d6d26de4224c9257fc78c3a92065d4ff
Pamf1973 <pedroprousa2023@gmail.com>
2025-06-15 12:29:58 -0400

    Add welcome.py

---
+++ welcome.py
@@ -0,0 +1,8 @@
+# welcome.py
+
+def main():
+    message = "Hello World, I am new to AI, I am here to learn and build with you in my new Repo."
+    print(message)
+
+if __name__ == "__main__":
+    main()
