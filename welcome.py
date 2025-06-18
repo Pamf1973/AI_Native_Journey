a59bf5e39c51b88a1c316b2f0feb36e3a4c9b5eb
Pamf1973 <pedroprousa2023@gmail.com>
2025-06-17 20:25:03 -0400

    Update welcome message in welcome.py

--- welcome.py
+++ welcome.py
@@ -1,7 +1,7 @@
 # welcome.py
 
 def main():
-    message = "Hello World, I am new to AI, I am here to learn and build with you in my new Repo."
+    message = "Hello, I am Pedro. I am new to AI, I am here to learn and build with you in my new Repo."
     print(message)
 
 if __name__ == "__main__":
